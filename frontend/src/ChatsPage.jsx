d4e4e69712f12ab1225b9103e73230efef499f41
EricSousa02 <ericsousadeandrade@gmail.com>
2023-07-26 09:31:03 -0300

    a

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -8,7 +8,7 @@ const ChatsPage = (props) => {
   return (
     <div className="background">
       <PrettyChatWindow
-        // projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+
         projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
         username={props.user.username}
         secret={props.user.secret}
